8e5dae725146e40905edf1e059b5ade6bfee1d51
martin mkolwe <martinjj80@gmail.com>
2022-01-17 21:44:20 +0000

    update menu

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.post('*',(req,res) => {
     // first level response
     if( text === ""){
         let response = `
-        CON Welcome to my simple USSD app. Select optionns from below
+        CON Welcome to my simple loan USSD App. Select optionns from below
         1. Take a loan
         2. Loan Balance
         3. Check Eligility
@@ -30,11 +30,11 @@ app.post('*',(req,res) => {
     } else if( text === '1') {
         // buz logic for first level
        let response = `
-       CON CHoose from the options below
-       1. NGN 1000
-       2. NGN 3000
-       3. NGN 5000
-       4. NGN 7000
+       CON Choose from the options below
+       1. KSH 1000
+       2. KSH 3000
+       3. KSH 5000
+       4. KSH 7000
        `;
        res.send(response);
 
@@ -51,22 +51,22 @@ app.post('*',(req,res) => {
 
     } else if(text === '1*1'){
         let balance = 1000;
-        let response = `END Your accout balance has been credited with NGN ${balance}`
+        let response = `END Your accout balance has been credited with KSH ${balance}`
         res.send(response);
 
     } else if(text === '1*2'){
         let balance = 3000;
-        let response = `END Your accout balance has been credited with NGN ${balance}`
+        let response = `END Your accout balance has been credited with KSH  ${balance}`
         res.send(response);
     
     } else if(text === '1*3'){
         let balance = 5000;
-        let response = `END Your accout balance has been credited with NGN ${balance}`
+        let response = `END Your accout balance has been credited with KSH ${balance}`
         res.send(response);
     
     } else if(text === '1*4'){
         let balance = 4000;
-        let response = `END Your accout balance has been credited with NGN ${balance}`
+        let response = `END Your accout balance has been credited with KSH ${balance}`
         res.send(response);
 
     } else {
