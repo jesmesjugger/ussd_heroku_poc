ac2c5f3b8bf730d13a61c3bec729d87d2280c35d
Martin Mkolwe <martinjj80@gmail.com>
2022-01-18 01:27:47 +0300

    Update app.js
    readability code

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import bodyParser from 'body-parser';
+
 import logger from 'morgan';
+
 import express from 'express';
+
 const app = express();
+
 const PORT = process.env.PORT || 3100;
-#testtest
+
 app.use(logger('dev'));
+
 app.use(bodyParser.json())
+
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
